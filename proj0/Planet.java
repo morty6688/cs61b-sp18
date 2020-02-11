3ba3070f97244ea4242b802dd71a4669c2565bcc
lijian12345 <1053294286@qq.com>
2020-02-11 16:14:15 +0800

    change G from public to private

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -7,7 +7,7 @@ public class Planet {
     public double mass;
     public String imgFileName;
 
-    static final double G = 6.67e-11;
+    private static final double G = 6.67e-11;
 
     public Planet(double xxPos, double yyPos, double xxVel, double yyVel, double mass, String imgFileName) {
         this.xxPos = xxPos;
