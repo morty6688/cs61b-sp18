46f2ab591c280749b8fd652740ab1f0f35551b6e
lijian12345 <1053294286@qq.com>
2020-02-20 21:46:22 +0800

    Upper case...

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -10,12 +10,12 @@ public class TestOffByOne {
     @Test
     public void testEqualChars() {
         assertTrue(offByOne.equalChars('a', 'b'));
-        assertTrue(offByOne.equalChars('r', 'q'));
         assertTrue(offByOne.equalChars('&', '%'));
-        assertTrue(offByOne.equalChars('A', 'B'));
         assertFalse(offByOne.equalChars('a', 'e'));
+        assertFalse(offByOne.equalChars('A', 'b'));
+        assertTrue(offByOne.equalChars('r', 'q'));
+        assertTrue(offByOne.equalChars('A', 'B'));
         assertFalse(offByOne.equalChars('z', 'a'));
         assertFalse(offByOne.equalChars('a', 'a'));
-        assertFalse(offByOne.equalChars('A', 'a'));
     }
 }
