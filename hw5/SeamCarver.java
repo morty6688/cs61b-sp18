432fcfcb3fe54bcc5be859c6befa76650392f358
lijian12345 <1053294286@qq.com>
2020-04-08 21:59:07 +0800

    copy constructor

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -16,7 +16,7 @@ public class SeamCarver {
 
     // current picture
     public Picture picture() {
-        return picture;
+        return new Picture(picture);
     }
 
     // width of current picture
